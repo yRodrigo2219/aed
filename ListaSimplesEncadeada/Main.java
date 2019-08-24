45d2bbf71595db0dd2ef1894fcf0cc8af09a1291
yRodrigo2219 <50002002+yRodrigo2219@users.noreply.github.com>
2019-08-24 11:00:29 -0300

    Update Main.java

--- ListaSimplesEncadeada/Main.java
+++ ListaSimplesEncadeada/Main.java
@@ -1,8 +1,35 @@
 public class Main{
     public static void main(String[] args) {
-        ListaEncadeada lista = new ListaEncadeada();
+        ListaEncadeada lista1 = new ListaEncadeada();
+        ListaEncadeada lista2 = new ListaEncadeada();
 
-        lista.adicionarInicio(1);
+        for(int i = 0; i <= 20; i++){
+            lista1.inserirFinal(20-i);
+            lista2.inserirFinal(i);
+        }
 
-    }
+        System.out.println("Lista 1: " + lista1);
+        System.out.println("Lista 2: " + lista2);
+
+        try{
+            // insere o 5 depois do 5
+            lista2.inserirDepois(5, 5);
+            lista2.inserirDepois(5, 5);
+
+            // insere o 3 antes do 1
+            lista2.inserirAntes(3, 1);
+
+            // extrai dois 5s da lista
+            lista2.extrair(5);
+            lista2.extrair(5);
+
+            // atribui a lista2 a lista1
+            lista1.atribuir(lista2);
+
+            System.out.println("Lista 1: " + lista1);
+            System.out.println("Lista 2: " + lista2);
+        }catch(Exception e){
+            System.out.println("" + e.getMessage());
+        }
+      }
 }
